9db3c337d5dec84b0573b6324f588e3c373f696f
Ilya Brant <detroitib31@gmail.com>
2022-11-08 16:20:50 +0600

    Написал вычисления и изменил "val n" на "var n"

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,8 +5,21 @@ fun main(args: Array<String>) {
     val reader = Scanner(System.`in`)
 
     print("Введите число: ")
-    val n = reader.nextDouble()
+    var n = reader.nextDouble()
 
     print("Перевести в байты(b) или килобайты(k): ")
     val c = reader.next()
+
+    if (c == "b") {
+        n *= 1024
+        print(n)
+        return
+    }
+    if (c == "k") {
+        n /= 1024
+        print(n)
+        return
+    }
+
+    print("Неверный ввод")
 }
