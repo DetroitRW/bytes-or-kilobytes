1593b87c5d8f8da525549edc5e9b0988049317cb
Ilya Brant <detroitib31@gmail.com>
2022-11-08 17:09:18 +0600

    Изменил с if на when

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,16 +10,10 @@ fun main(args: Array<String>) {
     print("Перевести в байты(b) или килобайты(k): ")
     val c = reader.next()
 
-    if (c == "b") {
-        n *= 1024
-        print(n)
-        return
-    }
-    if (c == "k") {
-        n /= 1024
-        print(n)
-        return
+    when (c) {
+        "b" -> n *= 1024
+        "k" -> n /= 1024
     }
 
-    print("Неверный ввод")
+    print(n)
 }
